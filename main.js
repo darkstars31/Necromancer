da2864885625e2e3719ccab64d6f47ebd973036b
Anthony Santi <darkstars31@gmail.com>
2019-02-18 16:33:10 -0600

    Updated files, Added flatfile LowDB and moved requires to config and dao

--- main.js
+++ main.js
@@ -1,43 +1,16 @@
 
 var config  = require('./config');
-var express = require('express');
-var log4js  = require('log4js');
-var app     = express();
-var helmet = require('helmet');
-var http = require('request-promise');
+var dao 	= require('./dao');
+var logger  = config.logger;
+var app = config.http;
 
-var logger = log4js.getLogger('api');
-log4js.configure(config.log4jsConfig);
 
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded());
-
-console.info(`Webhook Service is up on port: ${config.express.port}`);
-
-app.post("/v1/webhook", (req, res, next) => {
-
-	var options = {
-				method: "POST",
-				uri: ``,
-				body: {	value1: req.body.search_name },
-				json: true
-			};
-			http(options).then((result)=> {console.log('Notification Triggered: '+ req.body.search_name + ' @ ' + new Date().toUTCString())})
-			.catch((e) => { logger.error("Error Sending Notification: " + req.body.search_name)});
-			
-	res.send("true");
-});
+console.log(`Necromancer Service is up on port: ${config.express.port}`);
 
 app.post("/v1/deployhook", (req, res, next) => {
-	console.log(req.body);
-	logger.error(req.body);
-	var registeredProjects = {
-		SplunkWebhook: {
-			location: "C:\selfhealing\SplunkWebhook"
-		}
-	};
-
+	console.log("One Failed Request has been sent to the crypt.")
+	
+	logger.info(req.body);
 	
 });
 
